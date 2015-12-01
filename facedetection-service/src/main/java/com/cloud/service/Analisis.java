12a056f73f9923e69c2d56d5d95b38142bfd1f4c
Alejandro Pérez <alejandroperez@bitbucket.org>
2015-12-01 16:51:26 -0300

    Analisis

---
+++ facedetection-service/src/main/java/com/cloud/service/Analisis.java
@@ -0,0 +1,27 @@
+package com.cloud.service;
+
+import java.util.List;
+
+import com.cloud.dto.ProcessResult;
+
+public class Analisis {
+	
+	public static Resumen analizar(List<ProcessResult> resultados){
+		
+		/*Integer sumaParcial = 0;
+		Integer 
+		for(ProcessResult )*/
+		
+		
+			
+		int totalCarasDetectadas = resultados.stream().mapToInt(pr -> pr.getFaceCount()).sum();
+		
+		
+		
+		Resumen resumen = new Resumen();
+		
+		return new Resumen();
+		
+	}
+	
+}
---
+++ facedetection-service/src/main/java/com/cloud/service/Resumen.java
@@ -0,0 +1,28 @@
+package com.cloud.service;
+
+
+public class Resumen {
+	
+	private int cantCarasReferencia;
+	private int cantCarasDetectadas;
+
+	public Resumen(){}
+
+	public int getCantCarasReferencia() {
+		return cantCarasReferencia;
+	}
+
+	public void setCantCarasReferencia(int cantCarasReferencia) {
+		this.cantCarasReferencia = cantCarasReferencia;
+	}
+
+	public int getCantCarasDetectadas() {
+		return cantCarasDetectadas;
+	}
+
+	public void setCantCarasDetectadas(int cantCarasDetectadas) {
+		this.cantCarasDetectadas = cantCarasDetectadas;
+	}
+	
+	
+}
